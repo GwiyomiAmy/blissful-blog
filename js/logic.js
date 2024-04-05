a7cc48a6f126ec960122de1d1696395a9bfef226
gwiyomiamy <octipuss@gmail.com>
2024-04-05 00:29:13 -0700

    added footer

--- js/logic.js
+++ js/logic.js
@@ -1,8 +1,12 @@
+//reference to toggle button
 const toggle = document.querySelector('#theme');
 
+//set default mode to light
 let mode = "light"
 
-toggle.addEventListener('click', function(){
+//add function to toggle when clicked to switch between the dark and light class style elements
+toggle.addEventListener('click', function(e){
+   //check that button is working
    //console.log("button pressed")
    if (mode === "dark") {
       mode = "light";
